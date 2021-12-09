e201fbd64a33bd1299d695c88a896f20c8f8d882
TheinHtay <theinhtay@aceplussolutions.com>
2021-12-09 12:31:35 +0630

    Add test project

---
+++ Assets/VideoTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoTest : MonoBehaviour
+{
+    public GameObject background1;
+    public GameObject background2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickChangeButton()
+    {
+        Debug.Log("work");
+        if(background1.activeInHierarchy == true)
+        {
+            background1.SetActive(false);
+            background2.SetActive(true);
+        }
+        else
+        {
+            background2.SetActive(false);
+            background1.SetActive(true);
+        }
+    }
+}
