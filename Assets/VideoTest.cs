9fecfd983b5a38c58e0bb3e5226309815d5a9c87
TheinHtay <theinhtay@aceplussolutions.com>
2021-12-09 14:34:27 +0630

    Update background video

--- Assets/VideoTest.cs
+++ Assets/VideoTest.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Video;
 
 public class VideoTest : MonoBehaviour
 {
     public GameObject background1;
     public GameObject background2;
+    public RawImage rawImage1;
+    public RawImage rawImage2;
 
     // Start is called before the first frame update
     void Start()
@@ -27,18 +30,21 @@ public class VideoTest : MonoBehaviour
         {
             //background1.GetComponent<VideoPlayer>().Pause();
             //background2.GetComponent<VideoPlayer>().Play();
-
+            rawImage1.gameObject.SetActive(false);
             background1.SetActive(false);
 
-            //background2.SetActive(true);
+            rawImage2.gameObject.SetActive(true);
+            background2.SetActive(true);
 
         }
         else
         {
             //background2.GetComponent<VideoPlayer>().Pause();
             //background1.GetComponent<VideoPlayer>().Play();
-            //background2.SetActive(false);
+            rawImage2.gameObject.SetActive(false);
+            background2.SetActive(false);
 
+            rawImage1.gameObject.SetActive(true);
             background1.SetActive(true);
 
         }
